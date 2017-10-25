c493fd97855542145cec7d309d63e94ef295cfe5
eric.zhu <erzhu4@gmail.com>
2017-10-25 13:51:20 -0400

    commit for now

--- resources/assets/js/portfolio/MainNav.vue
+++ resources/assets/js/portfolio/MainNav.vue
@@ -2,12 +2,33 @@
     <div class="main-nav">
         <div class="row" style="height:200px;">
             <div class="container">
-                top part of the thing
+                <div class="row">
+                    TOP PART OF THE THING
+                </div>
             </div>
         </div>
         <div class="row">
             <div class="container">
-                BOTTOM PART OF THE THING
+                <div class="row">
+                    <div class="col-lg-2">
+                        Home
+                    </div>
+                    <div class="col-lg-2">
+                        About
+                    </div>
+                    <div class="col-lg-2">
+                        Games
+                    </div>
+                    <div class="col-lg-2">
+                        Projects
+                    </div>
+                    <div class="col-lg-2">
+                        Resume
+                    </div>
+                    <div class="col-lg-2">
+                        Contact
+                    </div>
+                </div>
             </div>
         </div>
     </div>

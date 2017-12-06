d25ec91f170c730f338f71022a67176dbcb51fa4
eric.zhu <erzhu4@gmail.com>
2017-12-06 10:00:09 -0500

    import galaxy

--- resources/assets/js/portfolio/Games.vue
+++ resources/assets/js/portfolio/Games.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="container" style="border:purple solid 1px; height:1500px;">
         <a href="#" @click="$emit('showOverlay', 'galaxy')">Galaxy</a>
+        <galaxy></galaxy>
         <a href="#" @click="$emit('showOverlay', 'snake')">Snake</a>
     </div>
 </template>
 
 <script>
+    import Galaxy from './games/galaxy/Galaxy.vue';
+
     export default {
+        components: {Galaxy}
 
     }
 </script>
---
+++ resources/assets/js/portfolio/games/galaxy/Galaxy.vue
@@ -0,0 +1,35 @@
+<template>
+    <div style="border:solid red 5px;">
+        <div class="score-container">Score: <div class="galaxy-score">0</div></div>
+        <canvas></canvas><br>
+        <button class="start-button">Start</button>
+        <button class="reset">Reset</button>
+        <div class="game-over hide"> GAME OVER!!!!! </div>
+    </div>
+</template>
+
+<script>
+    import Utils from './lib/util.js';
+    import MovingObjects from './lib/movingObject.js';
+    import ExpandingObjects from './lib/expandingObject.js';
+    import Enemy from './lib/enemy.js';
+    import Game from './lib/game.js';
+
+    export default {
+        mounted(){
+            this.initialize();
+        },
+
+        methods: {
+            initialize(){
+                console.log("initialize");
+                var canvasEl = document.getElementsByTagName("canvas")[0];
+                canvasEl.width = Galaxy.Game.DIM_X;
+                canvasEl.height = Galaxy.Game.DIM_Y;
+                var ctx = canvasEl.getContext("2d");
+//                var game = new Galaxy.Game();
+//                var view = new Galaxy.GameView(game, ctx);
+            }
+        }
+    }
+</script>

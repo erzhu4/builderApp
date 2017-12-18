a23c957c6a755669d15aec69a4022b18f5f3342c
eric.zhu <erzhu4@gmail.com>
2017-12-18 17:15:29 -0500

    add webpack config folder

---
+++ webpack/webpack.react.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+
+    entry: [
+        './resources/assets/js/test.js'
+    ],
+
+    output: {
+        filename: 'testbundle.js',
+        path: path.join(__dirname, '../public/js')
+    }
+
+};
